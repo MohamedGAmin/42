3f8408cc092ee9e10f490323db0051ab5ffc90f5
mohamedgamin <mohamedgamin.2@gmail.com>
2022-10-06 14:53:02 +0400

    add strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -1,29 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mamin <mamin@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mamin <mamin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/27 18:53:25 by mamin             #+#    #+#             */
-/*   Updated: 2022/10/06 14:37:12 by mamin            ###   ########.fr       */
+/*   Created: 2022/10/06 14:35:10 by mamin              #+#    #+#             */
+/*   Updated: 2022/10/06 14:35:11 by mamin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    size_t  i;
+    size_t i;
 
-	i = 0;
-	if (!dst && !src)
-		return (0);
-
-	while (i < n)
-	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
-	return (dst);
+    i=0;
+    while(i<n)
+    {
+        if (s1[i]!=s2[i])
+            return (s1[i]-s2[i]);
+        i++;
+    }
+    return(0);
 }
