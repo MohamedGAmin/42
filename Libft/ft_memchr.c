6dffc1d4f98e7719dac2b7e8f00454f2808f9821
mohamedgamin <mohamedgamin.2@gmail.com>
2022-10-07 21:09:33 +0400

    updated memchr

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,13 +6,22 @@
 /*   By: mamin <mamin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 20:50:09 by mamin             #+#    #+#             */
-/*   Updated: 2022/10/07 20:50:58 by mamin            ###   ########.fr       */
+/*   Updated: 2022/10/07 21:09:15 by mamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void *memchr(const void *s, int c, size_t n)
+void *ft_memchr(const void *s, int c, size_t n)
 {
+    size_t i;
 
+    i = 0;
+    while(i<n)
+    {
+        if (((unsigned char *)s)[i] == c)
+            return  ((void *)(s + i));
+        i++;
+    }
+    return 0;
     
 }
